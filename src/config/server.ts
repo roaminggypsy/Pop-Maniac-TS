ededfe030b5d1f9f5d150e76160f69f2c6fb0463
Linjun Ouyang <lynjunoyang@gmail.com>
2021-09-18 12:11:25 -0700

    added

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,11 +3,16 @@ import path from 'path';
 const app = express()
 const PORT : string|number = process.env.PORT || 5000;
 
-const buildPath = path.join(__dirname, '..', "..", "front", 'build');
-app.use(express.static(buildPath));
-
 app.use("*",(req, res) =>{
     res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
+
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('front/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT,() => console.log(`hosting @${PORT}`));
