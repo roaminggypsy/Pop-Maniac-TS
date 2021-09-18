787a5fe2b5729a3e6a43a9cf0b875e988a0838ff
Linjun Ouyang <lynjunoyang@gmail.com>
2021-09-18 14:50:58 -0700

    fixed deploy

--- src/config/server.ts
+++ src/config/server.ts
@@ -9,9 +9,9 @@ app.use("*",(req, res) =>{
 
 if(process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('front/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
